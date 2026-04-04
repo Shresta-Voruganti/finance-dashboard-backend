80637083773dd17d1198b4cad6027c0be73b3e4e
Shresta-Voruganti <22bd1a053z@gmail.com>
2026-04-04 18:58:00 +0530

    final fix in middleware

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-  console.error(err); // 🔥 ADD THIS
+  console.error(err); 
 
   res.status(err.status || 500).json({
     message: err.message || "Internal Server Error"
